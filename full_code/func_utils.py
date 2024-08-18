b7df3d6962b72f1567855d250cd78d4661e5e12c
reason <ujini777@naver.com>
2024-08-18 18:28:03 +0900

    음성녹음추가

--- full_code/func_utils.py
+++ full_code/func_utils.py
@@ -15,6 +15,8 @@ import sys
 from sensor_msgs.msg import NavSatFix
 import numpy as np
 import re
+import wave
+
 
 # YOLO 모델을 초기화하고 비디오에서 프레임을 읽는 기능을 제공
 # 비디오 파일에서 프레임을 일정한 간격으로 추출하여 처리할 수 있게 함
@@ -373,6 +375,41 @@ class FrameProcessor:
         return None
     
 #=========================== STT =============================
+
+def record_audio(seconds, filename="request.wav"):
+    #음성 녹음
+    chunk = 1024
+    format = pyaudio.paInt16
+    channels = 1
+    rate = 44100
+
+    p = pyaudio.PyAudio()
+
+    stream = p.open(format=format,
+                    channels=channels,
+                    rate=rate,
+                    input=True,
+                    frames_per_buffer=chunk)
+
+    print("Recording...")
+    frames = []
+
+    for _ in range(0, int(rate / chunk * seconds)):
+        data = stream.read(chunk)
+        frames.append(data)
+
+    print("Finished recording.")
+
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+    wf = wave.open(filename, 'wb')
+    wf.setnchannels(channels)
+    wf.setsampwidth(p.get_sample_size(format))
+    wf.setframerate(rate)
+    wf.writeframes(b''.join(frames))
+    wf.close()
 
 def recognize_speech_from_audio(filename):
     stt_client = speech.SpeechClient()
--- full_code/main.py
+++ full_code/main.py
@@ -1,4 +1,4 @@
-from func_utils import API, YOLOVideoCapture, FrameProcessor, text_to_speech_ssml, gps_sub, recognize_speech_from_audio, extract_bus_number, determine_intent
+from func_utils import API, YOLOVideoCapture, FrameProcessor, text_to_speech_ssml, gps_sub, recognize_speech_from_audio, extract_bus_number, determine_intent, record_audio
 import psycopg2
 import requests
 import xml.etree.ElementTree as ET
@@ -27,8 +27,11 @@ os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = '/home/minseokim521/catkin_ws/src
 ------------------------------------------------------------------------------------------------------------------------------------
 '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 
-# STT를 사용하여 사용자가 타고자 하는 버스 번호와 의도를 음성으로 입력 받음
-audio_filename = "user_input.wav"  # 음성 파일 경로
+# 음성을 녹음하여 WAV 파일로 저장
+audio_filename = "user_input.wav"
+record_audio(5, audio_filename)
+
+# 녹음된 오디오 파일에서 STT로 텍스트 인식
 recognized_text = recognize_speech_from_audio(audio_filename)
 
 # 인식된 텍스트 확인
@@ -46,7 +49,98 @@ if bus_number is None:
     msg = "버스 번호를 인식할 수 없습니다. 다시 시도해주세요."
     print(msg)
     text_to_speech_ssml(msg, "stt_error.mp3")
+    
+    # 음성 출력
+    sound = AudioSegment.from_mp3("stt_error.mp3")
+    sound.export("stt_error.wav", format="wav")
+    wave_obj = sa.WaveObject.from_wave_file("stt_error.wav")
+    play_obj = wave_obj.play()
+    play_obj.wait_done()
+
     exit()
+
+
+# GPS 데이터를 받아오고 그 값을 변수에 저장
+latitude, longitude = gps_sub()
+
+bus_api = API()
+
+# OCR을 사용하지 않고, STT로 인식된 버스 번호를 사용
+Bus_num = bus_number
+
+radius = 5
+
+# 가장 가까운 버스 정류장의 id 조회
+response1 = bus_api.station_pose(latitude, longitude, radius)
+root1 = ET.fromstring(response1)
+
+# 가장 가까운 버스정류장 선택
+station_list = bus_api.find_xml_val(root1, "arsId")
+print(station_list)
+
+# 첫번째 정류소가 가장 가까울 것이라고 가정
+routeid = station_list[0]
+
+# 데이터베이스에서 버스 번호와 정류소 이름에 해당하는 id값 가져오기
+bus_result = bus_api.database_query('bus', 'routeid', 'bus_id', Bus_num)
+station_list = bus_api.database_query_specific_column("station", 'node_id')
+station_name_list = bus_api.database_query_specific_column("station", 'station_name')
+X_locations = bus_api.database_query_specific_column("station", 'X_location')
+Y_locations = bus_api.database_query_specific_column("station", 'Y_location')
+
+# 리스트 평탄화
+X_locations = [x[0] for x in X_locations]
+Y_locations = [y[0] for y in Y_locations]
+
+# 가장 가까운 정류소 인덱스 찾기
+index = bus_api.find_nearest_index(latitude, longitude, X_locations, Y_locations)
+station_name = station_name_list[index]
+station_id = station_list[index]
+print(station_id, station_name)
+
+if bus_result == None:
+    print("STT로 인식된 버스 번호가 DB와 일치하지 않습니다.")
+    exit()
+
+# 정류소에 운행하는 버스정보 가져오기
+response2 = bus_api.station_bus_list(station_id[0])
+
+# xml 값을 가져옴
+root2 = ET.fromstring(response2)
+
+# 정류소에서 운행하는 버스 이름 정보 리스트
+bus_list = bus_api.find_xml_val(root2, "busRouteAbrv")
+
+# 버스 리스트에서 인식한 버스 정보가 있는지 찾음
+index, result = bus_api.find_api_val(bus_list, Bus_num)
+
+isArrive1 = []
+arrmsg1_list = []
+arrmsg2_list = []
+
+if result:
+    isArrive1 = bus_api.find_xml_val(root2, "isArrive1")
+    arrmsg1_list = bus_api.find_xml_val(root2, "arrmsg1")
+    arrmsg2_list = bus_api.find_xml_val(root2, "arrmsg2")
+else:
+    print("인식된 버스가 해당 정류소에서 운행되지 않습니다.")
+    exit()
+
+msg1, msg2, msg3 = 0, 0, 0
+
+# 변수 출력
+if not(isArrive1[index]):
+    msg1 = f"{Bus_num}번 버스가 도착했습니다."
+    print(msg1)
+else:
+    msg1 = f"{Bus_num}번 버스가 도착하지 않았습니다."
+    print(msg1)
+    
+msg2 = "첫번째 버스 도착 예정시간 :" + arrmsg1_list[index] 
+msg3 = "두번째 버스 도착 예정시간 :" + arrmsg2_list[index] 
+print(msg2)
+print(msg3)
+
 
 
 # 비디오 경로와 모델 경로 설정
